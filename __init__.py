87fd9247f21e80c86fdc08eef93885b1da9f2f9e
ESbros <edsg77@gmail.com>
2020-05-24 21:24:46 -0500

    _init_

--- __init__.py
+++ __init__.py
@@ -3,6 +3,6 @@ from os.path import dirname, join
 from textx import language, metamodel_from_file
 
 @language("ezjson", "*.ezjson")
-def turtle():
+def ezjson():
     "A language for creating JSON files easier."
     return metamodel_from_file(join(dirname(__file__), "ezjson.tx"))
