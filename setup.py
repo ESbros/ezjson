31f0d7846f18a6f54c3b1fb1837870ab60c642e1
ESbros <edsg77@gmail.com>
2020-05-24 21:21:50 -0500

    mod setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ AUTHOR_EMAIL = "edsg77@gmail.com"
 DESCRIPTION = "EZ-JSON is a domain-specific language to create JSON files in an easier way"
 KEYWORDS = "textX DSL python domain specific languages JSON files generation"
 LICENSE = "MIT"
-URL = "https://github.com/strumenta/..."
+URL = "https://github.com/ESbros/ezjson.git"
 setup(
     name=PACKAGE_NAME,
     version=VERSION,
@@ -23,7 +23,7 @@ setup(
     include_package_data=True,
     package_data={"": ["*.tx"]},
     install_requires=["textx_ls_core"],
-    entry_points={"textx_languages": ["turtle = tx_turtle:turtle"]},
+    entry_points={"textx_languages": ["ezjson = tx_ezjson:ezjson"]},
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
@@ -31,10 +31,6 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
     ],
 )
